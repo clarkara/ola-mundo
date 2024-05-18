5d34e3553d9e001125472b8349126f28ea7b5c38
clarkara <anacl4ra.ba@gmail.com>
2024-05-18 18:04:47 -0300

    eu sabo

---
+++ olaMundo/teste.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+    int primeiro, segundo;
+
+    std::cout << "Insira dois numeros para realizar a conta: \n";
+    std::cin >> primeiro;
+    std::cin >> segundo;
+
+    int mult = primeiro * segundo;
+    int div = primeiro / segundo;
+    int sub = primeiro - segundo;
+    int soma = primeiro + segundo;
+
+    std::cout << "\n" "O valor da multiplicacao eh de: \n" << mult;
+    std::cout << "\n" "O valor da divisao eh de: \n" << div;
+    std::cout << "\n" "O valor da soma eh de:\n" << soma;
+    std::cout << "\n" "O valor da subtracao eh de:\n" << sub;
+
+    std::cout << "\n";
+
+}
